10aab9d60540c95dba624834caec3345b4e72e6d
flemantico <flemantico@gmail.com>
2020-07-01 16:54:51 -0300

    init security

---
+++ com.amy.security/security/src/test/java/com/amy/security/security/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amy.security.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
