26b83ceda6e4ec65796a4b998fa07fe59f8a5c85
flemantico <flemantico@gmail.com>
2020-07-03 16:24:39 -0300

    update

--- backend/config_service/src/main/java/com/amy/config_service/ConfigServiceApplication.java
+++ backend/config_service/src/main/java/com/amy/config_service/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package com.amy.service;
+package com.amy.config_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ServiceApplication {
+public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceApplication.class, args);
+		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
 }
