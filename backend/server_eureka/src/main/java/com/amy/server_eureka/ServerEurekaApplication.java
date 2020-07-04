678be568f440c51ed0b10b6f639d1753b7456baa
flemantico <flemantico@gmail.com>
2020-07-04 15:39:32 -0300

    update

--- backend/server_eureka/src/main/java/com/amy/server_eureka/ServerEurekaApplication.java
+++ backend/server_eureka/src/main/java/com/amy/server_eureka/ServerEurekaApplication.java
@@ -2,8 +2,9 @@ package com.amy.server_eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-
+@EnableEurekaServer
 @SpringBootApplication
 public class ServerEurekaApplication {
 
