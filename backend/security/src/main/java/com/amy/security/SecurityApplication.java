f0f71e53418b07ced162e5430de87d3ed6607e9f
flemantico <flemantico@gmail.com>
2020-07-04 10:31:53 -0300

    update

--- backend/security/src/main/java/com/amy/security/SecurityApplication.java
+++ backend/security/src/main/java/com/amy/security/SecurityApplication.java
@@ -2,10 +2,17 @@ package com.amy.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+
+
+@EnableDiscoveryClient // ver si va
+@EnableFeignClients    // ver si va
+
 public class SecurityApplication {
 
 	public static void main(String[] args) {
