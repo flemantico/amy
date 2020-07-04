566889460c0eab6001281e3146320c6900a90878
flemantico <flemantico@gmail.com>
2020-07-04 15:35:12 -0300

    glade

---
+++ backend/server_config/src/test/java/com/amy/server_config/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amy.server_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
